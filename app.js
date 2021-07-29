759631041aaf94d0a1dccc95f208fcd00e799dc7
Sagnik Ghosh <64497647+sagnik11@users.noreply.github.com>
2021-07-29 17:35:34 +0530

    update

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var http = require("http");
-var express = require("express");
-var consolidate = require("consolidate"); //1
-var _ = require("underscore");
-var bodyParser = require('body-parser');
+const http = require('http');
+const express = require('express');
+const consolidate = require('consolidate');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-var routes = require('./routes'); //File that contains our endpoints
-var mongoClient = require("mongodb").MongoClient;
+const routes = require('./routes'); //File that contains our endpoints
+const socketEvents = require('./socket-events');
 
-var app = express();
+const app = express();
+
 app.use(bodyParser.urlencoded({
     extended: true,
 }));
@@ -16,53 +17,28 @@ app.use(bodyParser.json({
     limit: '5mb'
 }));
 
-app.set('views', 'views'); //Set the folder-name from where you serve the html page. 
-app.use(express.static('./public')); //setting the folder name (public) where all the static files like css, js, images etc are made available
+app.set('views', 'views'); // Set the folder-name from where you serve the html page.
+app.use(express.static('./public')); // setting the folder name (public) where all the static files like css, js, images etc are made available
 
 app.set('view engine', 'html');
-app.engine('html', consolidate.underscore); //Use underscore to parse templates when we do res.render
+app.engine('html', consolidate.handlebars); // Use handlebars to parse templates when we do res.render
 
-var server = http.Server(app);
-var portNumber = 8000; //for locahost:8000
-
-var io = require('socket.io')(server); //Creating a new socket.io instance by passing the HTTP server object
-
-server.listen(portNumber, function() { //Runs the server on port 8000
-    console.log('Server listening at port ' + portNumber);
-
-    var url = 'mongodb://localhost:27017/myUberApp'; //Db name
-    mongoClient.connect(url, function(err, db) { //a connection with the mongodb is established here.
-        console.log("Connected to Database");
-
-        app.get('/citizen.html', function(req, res) { //a request to /citizen.html will render our citizen.html page
-            //Substitute the variable userId in citizen.html with the userId value extracted from query params of the request.
-            res.render('citizen.html', {
-                userId: req.query.userId
-            });
-        });
-
-        app.get('/cop.html', function(req, res) {
-            res.render('cop.html', {
-                userId: req.query.userId
-            });
-        });
-
-        app.get('/data.html', function(req, res) {
-            res.render('data.html');
-        });
-
-        io.on('connection', function(socket) { //Listen on the 'connection' event for incoming sockets
-            console.log('A user just connected');
-
-            socket.on('join', function(data) { //Listen to any join event from connected users
-                socket.join(data.userId); //User joins a unique room/channel that's named after the userId 
-                console.log("User joined room: " + data.userId);
-            });
-
-            routes.initialize(app, db, socket, io); //Pass socket and io objects that we could use at different parts of our app
-        });
-    });
+// connect to Database
+const db = 'mongodb://localhost:27017/uberForX';
+mongoose.connect(db).then(value => {
+    // Successful connection
+    console.log(value.models);
+}).catch(error => {
+    // Error in connection
+    console.log(error);
 });
 
-/* 1. Not all the template engines work uniformly with express, hence this library in js, (consolidate), is used to make the template engines work uniformly. Altough it doesn't have any 
-modules of its own and any template engine to be used should be seprately installed!*/
+app.use('/', routes);
+
+const server = http.Server(app);
+const portNumber = 8000; // for locahost:8000
+
+server.listen(portNumber, () => { // Runs the server on port 8000
+    console.log(`Server listening at port ${portNumber}`);
+    socketEvents.initialize(server);
+});
